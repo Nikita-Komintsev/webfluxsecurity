79a7ad0f0c1c2bda36a0cdf5a6ee4477b833104a
nikita <nikita.comin@gmail.com>
2025-10-07 17:03:39 +0300

    JwtHandler

---
+++ src/main/java/com/proselyte/webfluxsecurity/security/JwtHandler.java
@@ -0,0 +1,51 @@
+package com.proselyte.webfluxsecurity.security;
+
+import com.proselyte.webfluxsecurity.exception.UnauthorizedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import reactor.core.publisher.Mono;
+
+import java.util.Base64;
+import java.util.Date;
+
+public class JwtHandler {
+
+    private final String secret;
+
+    public JwtHandler(String secret) {
+        this.secret = secret;
+    }
+
+    public Mono<VerificaionResult> check(String accessToken) {
+        return Mono.just(verify(accessToken))
+                .onErrorResume(e -> Mono.error(new UnauthorizedException(e.getMessage())));
+    }
+
+    private VerificaionResult verify(String token) {
+        Claims claims = getClaimsFromToken(token);
+        final Date expirationDate = claims.getExpiration();
+
+        if (expirationDate.before(new Date())) {
+            throw new RuntimeException("Expired JWT token");
+        }
+
+        return new VerificaionResult(claims, token);
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(Base64.getEncoder().encodeToString(secret.getBytes()))
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public static class VerificaionResult {
+        public Claims claims;
+        public String token;
+
+        public VerificaionResult(Claims claims, String token) {
+            this.claims = claims;
+            this.token = token;
+        }
+    }
+}
